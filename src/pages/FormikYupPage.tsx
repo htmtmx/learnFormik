3ea20c9d81f3b646ffcff2969e11afd980ff75c6
htmtmx <cesar.pineda@daksha.mx>
2024-09-25 16:05:10 -0600

    ✨ Include Yup validation schema in FormikYupPage component

--- src/pages/FormikYupPage.tsx
+++ src/pages/FormikYupPage.tsx
@@ -1,37 +1,10 @@
 import React from 'react';
-import { FormikErrors, useFormik } from 'formik';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 import './../styles/styles.css';
 
-interface FormValues{
-  firstName: string;
-  lastName: string;
-  email: string;
-}
-
 export const FormikYupPage = () => {
-
-  const validate = (values: FormValues) => {
-    const errors:FormikErrors<FormValues> = {};
-    if (!values.firstName) {
-      errors.firstName = 'First Name is required';
-    }else if(values.firstName.length < 3){
-      errors.firstName = 'First Name is too short';
-    }
-
-    if (!values.lastName) {
-      errors.lastName = 'Last Name is required';
-    } else if (values.lastName.length < 3) {
-      errors.lastName = 'Last Name is too short';
-    }
-
-    if (!values.email) {
-      errors.email = 'Email is required';
-    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-      errors.email = 'Invalid email address';
-    }
-    return errors;
-  }
 
   const { handleChange, values, handleSubmit, errors, touched, handleBlur } = useFormik({
     initialValues: {
@@ -42,7 +15,11 @@ export const FormikYupPage = () => {
     onSubmit: (values) => {
       console.log(values)
     },
-    validate
+    validationSchema: Yup.object({
+      firstName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+      lastName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
+      email: Yup.string().email('Invalid email address').required('Required'),
+    })
   });
 
   return (
@@ -59,7 +36,6 @@ export const FormikYupPage = () => {
           onBlur={handleBlur}
           onChange={handleChange}
           value={values.firstName} 
-          // className={errors.firstName && "has-error"}
         />
         {touched.firstName && errors.firstName && <span>{errors.firstName}</span>}
 
@@ -72,7 +48,6 @@ export const FormikYupPage = () => {
           onBlur={handleBlur}
           onChange={handleChange}
           value={values.lastName} 
-          // className={errors.lastName && "has-error"}
         />
         {touched.lastName && errors.lastName && <span>{errors.lastName}</span>}
 
@@ -85,7 +60,6 @@ export const FormikYupPage = () => {
           onBlur={handleBlur}
           onChange={handleChange}
           value={values.email} 
-          // className={errors.email && "has-error"}
         />
         {touched.email && errors.email && <span>{errors.email}</span>}
 
