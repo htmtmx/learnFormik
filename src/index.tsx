2a6e764daf53567cf9a764a04048f122f3e902f0
htmtmx <cesar.pineda@daksha.mx>
2024-09-26 17:53:14 -0600

    🚚 Refactor component imports in index.tsx and FormikAbstract.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { RegisterPage, FormikBasicPage, FormikYupPage, FormikComponents, FormikAbstract } from './pages';
+import {DynamicForm, RegisterPage, FormikBasicPage, FormikYupPage, FormikComponents, FormikAbstract, RegisterFormikPage } from "./pages";
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <FormikAbstract/>
+    <DynamicForm/>
   </React.StrictMode>
 );
 
